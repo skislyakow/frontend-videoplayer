e60e3dc65aacb2e89f8c248b7d7f9388811b2aa4
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-05-25 11:42:43 +0300

    using livereload

---
+++ server.py
@@ -0,0 +1,15 @@
+from livereload import Server
+
+
+def rebuild():
+    ...  # do things
+    print("Site rebuilt")
+
+
+rebuild()
+
+server = Server()
+
+server.watch("index.html", rebuild)
+
+server.serve(root=".")
